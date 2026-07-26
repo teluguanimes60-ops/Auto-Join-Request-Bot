1f05fe9cf4ba5c6baa30e1662113a2fe34853c13
teluguanimes60-ops <teluguanimes60@gmail.com>
2026-07-26 17:54:08 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,14 +1,9 @@
 from pyrogram import Client
-
 from config import config
-
 
 app = Client(
     "AutoJoinBot",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    workers=100,
-    sleep_threshold=60,
-    in_memory=False
+    bot_token=config.BOT_TOKEN
 )
